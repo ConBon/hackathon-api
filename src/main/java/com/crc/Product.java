6e2ec5484165918b167b67395d1585029f20f472
Conal McLaughlin <conalsmclaughlin@gmail.com>
2015-11-10 22:45:34 +0000

    added singleton object to handle db connections

--- src/main/java/com/crc/Product.java
+++ src/main/java/com/crc/Product.java
@@ -1,10 +1,20 @@
 package com.crc;
 
+import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+
+import com.crc.heroku.ConnectionProvider;
 
 /**
  * Root resource (exposed at "product" path)
@@ -12,6 +22,7 @@ import javax.ws.rs.core.MediaType;
 @Path("product/{productId}")
 public class Product {
 
+	private static ConnectionProvider connectionProvider = ConnectionProvider.getInstance();
     /**
      * Method handling HTTP GET requests. The returned object will be sent
      * to the client as "text/plain" media type.
@@ -19,8 +30,33 @@ public class Product {
      * @return String that will be returned as a text/plain response.
      */
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public String getProduct(@PathParam("productId") String productId) {
-        return "Hello, Heroku!";
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getProduct(@PathParam("productId") String productIdStr) {
+        int productId;
+        if(productIdStr != null && productIdStr != "") {	
+	        try {
+	        	productId = Integer.parseInt(productIdStr);
+	        } catch(NumberFormatException e) {}
+        }
+    	
+        
+        try {
+			Connection connection = connectionProvider.getConnection();
+			Statement stmt = connection.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT * FROM products;");
+			/*String results = "";
+			while (rs.next()) {
+				System.out.println("Thing: " + rs.getString("thing"));
+			}*/
+			return rs.getString(2);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+    	return "";
+    }
+    
+    @POST
+    public void postProduct(String x) {
+    	
     }
 }
--- src/main/java/com/crc/heroku/ConnectionProvider.java
+++ src/main/java/com/crc/heroku/ConnectionProvider.java
@@ -7,8 +7,25 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionProvider {
+	private static ConnectionProvider instance = null;
+	private static Connection connection = null;
 	
-	public static Connection getConnection() throws URISyntaxException, SQLException {
+	protected ConnectionProvider(){}
+	
+	public static ConnectionProvider getInstance() {
+		if(instance == null) {
+			instance = new ConnectionProvider();
+			try {
+				connection = instance.initialiseConnection();
+			} catch (URISyntaxException e) {
+				e.printStackTrace();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return instance;
+	}
+	public Connection initialiseConnection() throws URISyntaxException, SQLException {
 		URI dbUri = new URI(System.getenv("DATABASE_URL"));
 
 		String username = dbUri.getUserInfo().split(":")[0];
@@ -26,5 +43,12 @@ public class ConnectionProvider {
 				+ "?sslmode=require&ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory";
 		System.out.println(dbUrl);
 		return DriverManager.getConnection(dbUrl, username, password);
+	}
+	
+	public Connection getConnection() {
+		return connection;
+	}
+	public void setConnection(Connection connection) {
+		this.connection = connection;
 	}
 }
