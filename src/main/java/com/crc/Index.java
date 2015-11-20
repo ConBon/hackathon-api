9f99504e32d03545894c5e8499f0eb26b7e254c9
Conal McLaughlin <conalsmclaughlin@gmail.com>
2015-11-20 01:35:23 +0000

    adding doc at root

--- src/main/java/com/crc/Index.java
+++ src/main/java/com/crc/Index.java
@@ -12,11 +12,13 @@ import javax.ws.rs.core.MediaType;
  */
 @Path("/")
 public class Index {
-	
-	
+
 	@GET
     @Produces(MediaType.TEXT_PLAIN)
     public String getRoot() {
-		return "CRC Hackathon RESTful API demo";
+		return "CRC Hackathon RESTful API demo.\n"
+				+"@GET /products\n"
+				+"@GET /products/{productId}\n"
+				+"@GET /products/{productId}/reviews";
 	}
 }
